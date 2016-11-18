5cc318cb6e1e6e54e18089e588d47b8390b549a6
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-17 21:18:25 -0500

    Add styles, pass props to header

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+
 import './css/style.css';
 import App from './components/App';
 
-// import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker';
 
 render(<App />, document.querySelector('#main'));
