30291e21b054531fa3eaaa9751c7c5443915a833
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-18 19:30:09 -0500

    Add fish state, initial data load

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,7 +4,44 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
+    constructor() {
+        super(); //Now we can use 'this'
+
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+
+        //Initial state
+        this.state={
+            fishes: {},
+            orders: {}
+        }
+    }
+
+    addFish( fish ) {
+        // Copy state
+        const fishes = { ...this.state.fishes };
+
+        // Create timestamp for object id uniqueness
+
+        const timestamp = Date.now();
+
+        // Update state
+        fishes[`fish-${timestamp}`] = fish;
+
+        // Set state
+        this.setState({ fishes });
+    }
+
+    loadSamples() {
+        console.log('Loading sample fishes!');
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +49,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
         )
     }
